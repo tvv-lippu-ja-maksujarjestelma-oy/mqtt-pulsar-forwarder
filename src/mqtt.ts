b0f6df12373eadec89f28b4d39aa51f9f3b0c51b
haphut <haphut@mistmap.com>
2022-02-15 15:09:09 +0200

    fix: Rewrite async function as normal callback

--- src/mqtt.ts
+++ src/mqtt.ts
@@ -11,17 +11,24 @@ const createMqttClient = (
   logger.info("Connect to MQTT broker");
   const mqttClient = mqtt.connect(url, clientOptions);
 
-  mqttClient.on("message", async (topic, message, packet) => {
-    await pulsarProducer.send({
-      data: message,
-      properties: {
-        mqttTopic: topic,
-        mqttQos: packet.qos.toFixed(0),
-        mqttIsRetained: packet.retain.toString(),
-        mqttIsDuplicate: packet.dup.toString(),
-      },
-      eventTimestamp: Date.now(),
-    });
+  mqttClient.on("message", (topic, message, packet) => {
+    pulsarProducer
+      .send({
+        data: message,
+        properties: {
+          mqttTopic: topic,
+          mqttQos: packet.qos.toFixed(0),
+          mqttIsRetained: packet.retain.toString(),
+          mqttIsDuplicate: packet.dup.toString(),
+        },
+        eventTimestamp: Date.now(),
+      })
+      .then(
+        () => {},
+        (err) => {
+          logger.error({ err }, "Publishing to Pulsar failed");
+        }
+      );
   });
 
   mqttClient.on("connect", () => {
